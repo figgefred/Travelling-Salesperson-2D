d56c75119fd97b0fd5fe396197ac1743e5a05ef1
Joakim Uddholm <joakim@uddholm.com>
2013-12-02 22:48:03 +0100

    Basic CSV print. Pipe to file and do what you need.

--- runalltest.py
+++ runalltest.py
@@ -24,23 +24,33 @@ if len(f) == 0:
 	print "No tests found! Please run mktests.py"
 	exit(1)
 
-print "Found", len(f), "tests."	
+print "Found", len(f)-1, "tests."	
 
-distances = list()
-times = list()
+distances = [0] * (len(f)-1)
+times = [0] * (len(f)-1)
+errors = list()
 for test in f:
 	if test == ".gitignore":
 		continue
 	print "Running test:", test	
-	try:
-		distance, time = runtest.runtest(sys.argv[1], testdir+test)
-		distances.append(distance)
-		times.append(time)
+	try:		
+		t = int(test)
+		distance, time = runtest.runtest(sys.argv[1], testdir+str(t))
+		distances[t] = distance
+		times[t] = time		
 	except Exception as ex:
-		print ex
+		errors.append(test + ": " + ex)
 		
 
-print 
+for i in range(len(times)):
+	print str(i)+","+str(distances[i])+","+str(times[i])
+
+if len(errors) > 0:
+	print "Errors occured on the following tests: " 
+	for err in errors:
+		print err
+
+print
 print "Average distance: ", sum(distances) / len(distances)
 print "Average time: ", sum(times) / len(times)
 
